c39a5823f521d3e4408b65a9892ae642637f9db7
jhg1126q <98995858+jhg1126q@users.noreply.github.com>
2023-11-23 12:58:23 +0900

    🚨code 정리
    prettier 적용

--- src/TypingMain.js
+++ src/TypingMain.js
@@ -2,110 +2,108 @@ import { useState } from "react";
 import "./main.css";
 
 const TypingMain = () => {
+  // 변수
+  const [state, setState] = useState({
+    typeContentMsg: "",
+  });
+  // state 하나로 관리하기가 안됨
+  const [typeAccuracy, setTypeAccuracy] = useState(0);
+  const [typeSpeed, setTypeSpeed] = useState(0);
+  const typeResultMsg = "타이핑 테스트 목록";
 
-    // 변수
-    const [state, setState] = useState({
-        typeContentMsg : ""
-    });
-    // state 하나로 관리하기가 안됨
-    const [typeAccuracy, setTypeAccuracy] = useState(0);
-    const [typeSpeed, setTypeSpeed] = useState(0);
-    const typeResultMsg = "타이핑 테스트 목록";
-
-    /*********************************************
+  /*********************************************
         title : handleChangeState
         param : event
         content : 변수 변경시 데이터 업데이트 반영
     **********************************************/
-    const handleChangeState = (e) => {
+  const handleChangeState = (e) => {
+    let contents = e.target.value;
 
-        let contents = e.target.value;
-        
-        if(e.target.name === "typeContentMsg") {
-            checkTypingContent(contents);
-        }
+    if (e.target.name === "typeContentMsg") {
+      checkTypingContent(contents);
+    }
 
-        setState({
-            ...state,
-            [e.target.name] : contents
-        });
-    };
+    setState({
+      ...state,
+      [e.target.name]: contents,
+    });
+  };
 
-    /*********************************************
+  /*********************************************
         title : handleChangeState
         param : event
         content : typingContent 칸 제어
     **********************************************/
-    const checkTypingContent = (txt) => {
-        if(typeResultMsg.length <= txt.length) {
-            if(typeResultMsg === txt){
-                setTypeSpeed(10);
-                setTypeAccuracy(100);
-                txt = "";
-            } else {
-                // 작성중인 상태 일수 도 있다.
-                // 해당 판별 여부를 위해 정확도를 측정하지 않아야합니다
-                console.log("실패 ");
-                setTypeSpeed(10);
-                setTypeAccuracy(0);
-            }
-        } else if(typeResultMsg.length <= txt.length) {
-            // 작성 완료인 상태 
-            // 텍스트 잘라서 비교해야한다
-        } else {
-            console.log("작성중");
-        }
-    };
+  const checkTypingContent = (txt) => {
+    if (typeResultMsg.length <= txt.length) {
+      if (typeResultMsg === txt) {
+        setTypeSpeed(10);
+        setTypeAccuracy(100);
+        txt = "";
+      } else {
+        // 작성중인 상태 일수 도 있다.
+        // 해당 판별 여부를 위해 정확도를 측정하지 않아야합니다
+        console.log("실패 ");
+        setTypeSpeed(10);
+        setTypeAccuracy(0);
+      }
+    } else if (typeResultMsg.length <= txt.length) {
+      // 작성 완료인 상태
+      // 텍스트 잘라서 비교해야한다
+    } else {
+      console.log("작성중");
+    }
+  };
 
-    /*********************************************
+  /*********************************************
         title : handlePageMove
         param : event
         content : 
     **********************************************/
-    const handlePageMove = () => {
-        console.log("페이지 이동 ::: 추가 삭제");
-    };
+  const handlePageMove = () => {
+    console.log("페이지 이동 ::: 추가 삭제");
+  };
 
-    /*********************************************
+  /*********************************************
         title : handleKeyDown
         param : event
         content : 입력되는 키 구분
     **********************************************/
-    const handleKeyDown = (e) => {
-        if(e.keyCode === 13) {
-            console.log("Enter");
-        } else if(e.keyCode === 32){
-            console.log("Space");
-        };
-    };
-    
-    // 타이핑 성공시 텍스트 인풋 초기화 해야한다
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      console.log("Enter");
+    } else if (e.keyCode === 32) {
+      console.log("Space");
+    }
+  };
 
-    return (
+  // 타이핑 성공시 텍스트 인풋 초기화 해야한다
+
+  return (
     <div className="TypingMain">
-        <h2>사이트 메인화면</h2>
-        <div>
-            <p>{typeResultMsg}</p>
-        </div>
-        <div>
-            <input 
-                name="typeContentMsg" 
-                value={state.typeContentMsg}
-                onChange={handleChangeState}
-                onKeyDown={handleKeyDown}>
-            </input>
-        </div>
-        <div>
-            <p className="infotxt">속도 : {typeSpeed} / min</p>
-        </div>
-        <div>
-            <p className="infotxt">정확도 : {typeAccuracy} %</p>
-        </div>
-        <div>
-            <button onClick={handlePageMove}>Add / Delete</button>
-        </div>
-    </div>    
-    );
-}
+      <h2>사이트 메인화면</h2>
+      <div>
+        <p>{typeResultMsg}</p>
+      </div>
+      <div>
+        <input
+          name="typeContentMsg"
+          value={state.typeContentMsg}
+          onChange={handleChangeState}
+          onKeyDown={handleKeyDown}
+        ></input>
+      </div>
+      <div>
+        <p className="infotxt">속도 : {typeSpeed} / min</p>
+      </div>
+      <div>
+        <p className="infotxt">정확도 : {typeAccuracy} %</p>
+      </div>
+      <div>
+        <button onClick={handlePageMove}>Add / Delete</button>
+      </div>
+    </div>
+  );
+};
 
 export default TypingMain;
